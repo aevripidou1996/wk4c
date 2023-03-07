8c197f68d123335b4acfd45052302bf1650f148f
Andrew Evripidou <aevripidou1996@gmail.com>
2023-03-08 01:15:56 +1100

    needs to be debuged

--- assets/script.js
+++ assets/script.js
@@ -36,3 +36,66 @@ var quizData = [
 
 ];
 
+var quiz= document.getElementById('quiz')
+var answerEls = document.querySelectorAll('.answer')
+var questionE1= document.getElementById('question')
+var a_text = document.getElementById('a_text')
+var b_text = document.getElementById('b_text')
+var c_text = document.getElementById('c_text')
+var d_text = document.getElementById('d_text')
+var submitbtn = document.getElementById('submit')
+
+
+var currentQuiz = 0
+var score = 0
+
+
+
+function loadQuiz(){
+    deselectAnsers()
+
+    var currentQuizData = quizData[currentQuiz]
+
+    questionE1.innerText = currentQuizData.question
+    a_text.innerHTML = currentQuizData.a
+    b_text.innerHTML = currentQuizData.b
+    c_text.innerHTML = currentQuizData.c
+    d_text.innerHTML = currentQuizData.d
+
+}
+
+function deselectAnsers(){
+    answerEls.forEach(answerEls => answerEls.checked = false)
+}
+
+function getSelected(){
+    var answerEls
+    answerEls.forEach(answerEls => {
+        if(answerEls.checked) {
+            answer = answerEls.id
+        }
+    })
+    return answer
+}
+
+submitbtn.addEventListener('click', () => {
+    var answer = getSelected()
+    if(answer){
+        if(answer === quizData[currentQuiz].correct) {
+            score++
+        }
+
+        currentQuiz++
+        if(currentQuiz < quizData.length){
+            loadQuiz()
+        }else
+        quizinnerHTML = 
+        <h2> You Answered ${score}/${quizData.length} questions correctley</h2>
+
+        <button >onclick="location.reload()" reload</button>
+
+
+        
+        
+    }
+})
